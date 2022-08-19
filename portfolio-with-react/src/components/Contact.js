34b4265b127ced592a5426174fb1a628c6947206
huntersteffner <huntersteffner@gmail.com>
2022-08-19 09:50:53 -0400

    Deployed website

--- portfolio-with-react/src/components/Contact.js
+++ portfolio-with-react/src/components/Contact.js
@@ -2,7 +2,17 @@ export default function Contact() {
     // All functional components must have a return method that contains JSX.
     // We return all the JSX inside a parent element with a className of "container".
     return (
-        <h1>Contact</h1>
+        <div>
+            <h1>Contact</h1>
+            <p>Send Email</p>
+            
+            <form className="contact-form" action="contactform.php"><input type="text" name="name" placeholder="Full Name" /></form>
+            <form className="contact-form" action="contactform.php"><input type="text" name="mail" placeholder="Your email" /></form>
+            <form className="contact-form" action="contactform.php"><input type="text" name="subject" placeholder="Subject" /></form>
+            <textarea name="message" placeholder="Message" cols="30" rows="10"></textarea>
+            <button type="submit" name="submit">Send Mail</button>
+
+        </div>
 
     )
   }
--- portfolio-with-react/src/components/Work.js
+++ portfolio-with-react/src/components/Work.js
@@ -1,8 +1,21 @@
 export default function Work() {
-    // All functional components must have a return method that contains JSX.
-    // We return all the JSX inside a parent element with a className of "container".
-    return (
-        <h1>Work</h1>
-
-    )
-  }
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <div>
+      <div id="project-list">
+        <div className="project">
+          <div className="project-image">
+            <img src="/Wookies-Gold.png" alt="" />
+          </div>
+          <div class="project-text">
+            <div class="project-text-wrap">
+              <p class="project-text-category">Node.js/MySQL</p>
+              <h2 class="project-text-title">Wookies Gold</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
