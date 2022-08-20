fcdc258489fc6f26b30ed964a4f800b644a4199f
huntersteffner <huntersteffner@gmail.com>
2022-08-19 21:26:13 -0400

    Added formsubmit.co to contact page

--- portfolio-with-react/src/components/Contact.js
+++ portfolio-with-react/src/components/Contact.js
@@ -1,18 +1,17 @@
 export default function Contact() {
-    // All functional components must have a return method that contains JSX.
-    // We return all the JSX inside a parent element with a className of "container".
-    return (
-        <div>
-            <h1>Contact</h1>
-            <p>Send Email</p>
-            
-            <form className="contact-form" action="contactform.php"><input type="text" name="name" placeholder="Full Name" /></form>
-            <form className="contact-form" action="contactform.php"><input type="text" name="mail" placeholder="Your email" /></form>
-            <form className="contact-form" action="contactform.php"><input type="text" name="subject" placeholder="Subject" /></form>
-            <textarea name="message" placeholder="Message" cols="30" rows="10"></textarea>
-            <button type="submit" name="submit">Send Mail</button>
-
-        </div>
-
-    )
-  }
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <div>
+      <form action="https://formsubmit.co/acaa027063cedb37352926a943777828 " method="POST">
+        <input type="hidden" name="_subject" value="New email from Portfolio" />
+        <input type="text" name="name" placeholder="Name" required />
+        <input type="email" name="email" placeholder="Email Address" required/>
+        <input type="text" name="message" placeholder="write your message here." />
+        <button type="submit">Send</button>
+      </form>
+      
+      
+    </div>
+  )
+}
