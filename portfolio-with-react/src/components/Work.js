564fd173bff7b6fab9f680f2e60d9ec52647d8c5
huntersteffner <huntersteffner@gmail.com>
2022-08-23 10:41:42 -0400

    Added projects to work page

--- portfolio-with-react/src/components/Work.js
+++ portfolio-with-react/src/components/Work.js
@@ -4,15 +4,70 @@ export default function Work() {
   return (
     <div>
       <div id="project-list">
-        <div className="project">
-          <div className="project-image">
-            <img src="/Wookies-Gold.png" alt="" />
+        <div className="project-card">
+          <div className="project-img">
+            <img src="./Wookies-Gold.png" alt="Wookies Gold" />
           </div>
-          <div class="project-text">
-            <div class="project-text-wrap">
-              <p class="project-text-category">Node.js/MySQL</p>
-              <h2 class="project-text-title">Wookies Gold</h2>
-            </div>
+          <div className="project-desc">
+            <h3>Wookies Gold</h3>
+            <h4>Node.js, MySQL, Bootstrap</h4>
+            <a className="btn btn-primary" href="">Deployed Site</a>
+            <a className="btn btn-primary" href="">GitHub</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-img">
+            <img src="./Weather-App.png" alt="Weather App" />
+          </div>
+          <div className="project-desc">
+            <h3>Weather App</h3>
+            <h4>JavaScript, Bootstrap</h4>
+            <a className="btn btn-primary" href="">Deployed Site</a>
+            <a className="btn btn-primary" href="">GitHub</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-img">
+            <img src="./Concert-Finder.png" alt="Concert Finder" />
+          </div>
+          <div className="project-desc">
+            <h3>Concert Finder</h3>
+            <h4>Javascript, Bootstrap, Leaflet</h4>
+            <a className="btn btn-primary" href="">Deployed Site</a>
+            <a className="btn btn-primary" href="">GitHub</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-img">
+            <img src="./Social-Blog.png" alt="Social Blog" />
+          </div>
+          <div className="project-desc">
+            <h3>Social Blog</h3>
+            <h4>Node.js, MySQL, Bootstrap</h4>
+            <a className="btn btn-primary" href="">Deployed Site</a>
+            <a className="btn btn-primary" href="">GitHub</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-img">
+            <img src="./Work-Team-Generator.png" alt="Work Team Generator" />
+          </div>
+          <div className="project-desc">
+            <h3>Work Team Generator</h3>
+            <h4>Node.js, Bootstrap, Inquirer</h4>
+            <a className="btn btn-primary" href="">Deployed Site</a>
+            <a className="btn btn-primary" href="">GitHub</a>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-img">
+            <img src="./Password-Generator.png" alt="Password Generator" />
+          </div>
+          <div className="project-desc">
+            <h3>Password Generator</h3>
+            <h4>HTML, CSS, JavaScript</h4>
+            <a className="btn btn-primary" href="">Deployed Site</a>
+            <a className="btn btn-primary" href="">GitHub</a>
           </div>
         </div>
       </div>
