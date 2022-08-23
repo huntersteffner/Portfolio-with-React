cb80e47952b2f55fe4fdc8bb3b76c6812cb7e924
huntersteffner <huntersteffner@gmail.com>
2022-08-23 11:13:24 -0400

    Projects now linking properly

--- portfolio-with-react/src/components/Work.js
+++ portfolio-with-react/src/components/Work.js
@@ -5,69 +5,105 @@ export default function Work() {
     <div>
       <div id="project-list">
         <div className="project-card">
-          <div className="project-img">
-            <img src="./Wookies-Gold.png" alt="Wookies Gold" />
+          <div className="project-img-container">
+            <div className="project-img">
+              <img src="./Wookies-Gold.png" alt="Wookies Gold" />
+            </div>
           </div>
           <div className="project-desc">
             <h3>Wookies Gold</h3>
             <h4>Node.js, MySQL, Bootstrap</h4>
-            <a className="btn btn-primary" href="">Deployed Site</a>
-            <a className="btn btn-primary" href="">GitHub</a>
+            <a className="btn btn-primary" target="_blank" href="https://glacial-tor-15581.herokuapp.com/">
+              Deployed Site
+            </a>
+            <a className="btn btn-primary" target="_blank" href="https://github.com/pcondori1996/WookiesGold">
+              GitHub
+            </a>
           </div>
         </div>
         <div className="project-card">
-          <div className="project-img">
-            <img src="./Weather-App.png" alt="Weather App" />
+          <div className="project-img-container">
+            <div className="project-img">
+              <img src="./Weather-App.png" alt="Weather App" />
+            </div>
           </div>
           <div className="project-desc">
             <h3>Weather App</h3>
             <h4>JavaScript, Bootstrap</h4>
-            <a className="btn btn-primary" href="">Deployed Site</a>
-            <a className="btn btn-primary" href="">GitHub</a>
+            <a className="btn btn-primary" target="_blank" href="https://github.com/huntersteffner/Weather-App">
+              Deployed Site
+            </a>
+            <a className="btn btn-primary" target="_blank" href="https://huntersteffner.github.io/Weather-App/">
+              GitHub
+            </a>
           </div>
         </div>
         <div className="project-card">
-          <div className="project-img">
-            <img src="./Concert-Finder.png" alt="Concert Finder" />
+          <div className="project-img-container">
+            <div className="project-img">
+              <img src="./Concert-Finder.png" alt="Concert Finder" />
+            </div>
           </div>
           <div className="project-desc">
             <h3>Concert Finder</h3>
             <h4>Javascript, Bootstrap, Leaflet</h4>
-            <a className="btn btn-primary" href="">Deployed Site</a>
-            <a className="btn btn-primary" href="">GitHub</a>
+            <a className="btn btn-primary" target="_blank" href="https://huntersteffner.github.io/Concert-Finder/">
+              Deployed Site
+            </a>
+            <a className="btn btn-primary" target="_blank" href="https://github.com/huntersteffner/Concert-Finder">
+              GitHub
+            </a>
           </div>
         </div>
         <div className="project-card">
-          <div className="project-img">
-            <img src="./Social-Blog.png" alt="Social Blog" />
+          <div className="project-img-container">
+            <div className="project-img">
+              <img src="./Social-Blog.png" alt="Social Blog" />
+            </div>
           </div>
           <div className="project-desc">
             <h3>Social Blog</h3>
             <h4>Node.js, MySQL, Bootstrap</h4>
-            <a className="btn btn-primary" href="">Deployed Site</a>
-            <a className="btn btn-primary" href="">GitHub</a>
+            <a className="btn btn-primary" target="_blank" href="https://github.com/huntersteffner/Social-Blog-Website">
+              Deployed Site
+            </a>
+            <a className="btn btn-primary" target="_blank" href="https://social-blog-steffner.herokuapp.com/">
+              GitHub
+            </a>
           </div>
         </div>
         <div className="project-card">
-          <div className="project-img">
-            <img src="./Work-Team-Generator.png" alt="Work Team Generator" />
+          <div className="project-img-container">
+            <div className="project-img">
+              <img src="./Work-Team-Generator.png" alt="Work Team Generator" />
+            </div>
           </div>
           <div className="project-desc">
             <h3>Work Team Generator</h3>
             <h4>Node.js, Bootstrap, Inquirer</h4>
-            <a className="btn btn-primary" href="">Deployed Site</a>
-            <a className="btn btn-primary" href="">GitHub</a>
+            <a className="btn btn-primary" target="_blank" href="https://github.com/huntersteffner/Work-Team-Generator">
+              Deployed Site
+            </a>
+            <a className="btn btn-primary" target="_blank" href="https://huntersteffner.github.io/Work-Team-Generator/">
+              GitHub
+            </a>
           </div>
         </div>
         <div className="project-card">
-          <div className="project-img">
-            <img src="./Password-Generator.png" alt="Password Generator" />
+          <div className="project-img-container">
+            <div className="project-img">
+              <img src="./Password-Generator.png" alt="Password Generator" />
+            </div>
           </div>
           <div className="project-desc">
             <h3>Password Generator</h3>
             <h4>HTML, CSS, JavaScript</h4>
-            <a className="btn btn-primary" href="">Deployed Site</a>
-            <a className="btn btn-primary" href="">GitHub</a>
+            <a className="btn btn-primary" target="_blank" href="https://huntersteffner.github.io/Password-Generator/">
+              Deployed Site
+            </a>
+            <a className="btn btn-primary" target="_blank" href="https://github.com/huntersteffner/Password-Generator">
+              GitHub
+            </a>
           </div>
         </div>
       </div>
